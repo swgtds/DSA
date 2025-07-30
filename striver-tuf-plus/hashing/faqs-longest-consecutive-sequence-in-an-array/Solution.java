597f77149c0876c41e2475968aaef2f469eabde3
swgtds <swagatadas.sd@outlook.com>
2025-07-30 16:01:41 +0530

    Medium Problem

---
+++ striver-tuf-plus/hashing/faqs-longest-consecutive-sequence-in-an-array/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int count, longest=1,x;
+        Set<Integer> set = new HashSet<>();
+        for(int i =0; i< nums.length; i++){
+            set.add(nums[i]);
+        }
+
+        for(Integer it: set){
+
+            if(!set.contains(it-1)){
+                x=it;
+                count =1;
+                while(set.contains(x+1)){
+                    count+=1;
+                    x+=1;
+
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+
+    }
+}
